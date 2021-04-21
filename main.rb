e2a06cfa32eb38fe989b2bdfbe8766be955dd877
hikimochi <rhikimochi@gmail.com>
2021-04-21 11:46:58 +0900

    Add reduce

--- main.rb
+++ main.rb
@@ -1,4 +1,9 @@
 class Number < Struct.new(:value)
+
+  def reducible?
+    false
+  end
+
   def to_s
     value.to_s
   end
@@ -9,6 +14,21 @@ class Number < Struct.new(:value)
 end
 
 class Add < Struct.new(:left, :right)
+
+  def reduce
+    if left.reducible?
+      Add.new(left.reduce, right)
+    elsif right.reducible?
+      Add.new(left, right.reduce)
+    else
+      Number.new(left.value + right.value)
+    end
+  end
+      
+  def reducible?
+    true
+  end
+
   def to_s
     "#{left} + #{right}"
   end
@@ -19,6 +39,21 @@ class Add < Struct.new(:left, :right)
 end
 
 class Multiply < Struct.new(:left, :right)
+
+  def reduce
+    if left.reducible?
+      Multiply.new(left.reduce, right)
+    elsif right.reducible?
+      Multiply.new(left, right.reduce)
+    else
+      Number.new(left.value * right.value)
+    end
+  end
+
+  def reducible?
+    true
+  end
+
   def to_s
     "#{left} * #{right}"
   end
@@ -28,9 +63,15 @@ class Multiply < Struct.new(:left, :right)
   end
 end
 
-Add.new(
+
+p expression = Add.new(
   Multiply.new(Number.new(1), Number.new(2)),
   Multiply.new(Number.new(3), Number.new(4))
 )
-
-Number.new(5)
+p expression.reducible?
+p expression = expression.reduce
+p expression.reducible?
+p expression = expression.reduce
+p expression.reducible?
+p expression = expression.reduce
+p expression.reducible?
